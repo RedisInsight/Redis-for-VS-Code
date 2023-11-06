ce7a3874dc7493e47e11f2305e1a2b0b01814b93
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2023-11-06 11:46:24 +0100

    fix pr comments

--- src/webviews/src/constants/core/app.ts
+++ src/webviews/src/constants/core/app.ts
@@ -1,6 +1,8 @@
+import * as l10n from '@vscode/l10n'
+
 export enum SortOrder {
   ASC = 'ASC',
   DESC = 'DESC',
 }
 
-export const DEFAULT_ERROR_MESSAGE = 'Something was wrong!'
+export const DEFAULT_ERROR_MESSAGE = l10n.t('Something was wrong!')
