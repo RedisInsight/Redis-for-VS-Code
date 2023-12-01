29380d321a47192c068c48fe00e8e2ea1e0aea7c
Tsikhan Koktysh <Tsikhan.Koktysh@softeq.com>
2023-12-01 17:44:00 +0200

    #RIV-69 - fixed cli paste issue

--- src/webviews/src/components/ContentEditable.tsx
+++ src/webviews/src/components/ContentEditable.tsx
@@ -17,13 +17,18 @@ const useRefCallback = <T extends any[]>(
   }, [])
 }
 
-const onPaste = (e: React.ClipboardEvent) => {
+const onPaste = async (e: React.ClipboardEvent) => {
   e.preventDefault()
-
-  const clipboardData = e.clipboardData || window.Clipboard || e.originalEvent.clipboardData
+  const clipboardData = e.clipboardData || window.Clipboard
   const text = clipboardData.getData('text/plain') as string
 
-  document.execCommand('insertText', false, parsePastedText(text))
+  const selection = window.getSelection()
+  const range = selection?.getRangeAt(0)
+  range?.deleteContents()
+  range?.insertNode(document.createTextNode(parsePastedText(text)))
+  range?.collapse(false)
+  selection?.removeAllRanges()
+  selection?.addRange(range as Range)
 }
 
 export function ContentEditable({
