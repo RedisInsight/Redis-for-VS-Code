72217413997ee1b3cbf6cc1ab5bf1a59e238d7ae
vlad-dargel <u.darhel@aisnovations.com>
2023-12-08 13:48:27 +0100

    add api tests for keys

--- tests/e2e/src/helpers/types/types.ts
+++ tests/e2e/src/helpers/types/types.ts
@@ -100,3 +100,125 @@ export type SSHParameters = {
   sshPrivateKey?: string
   sshPassphrase?: string
 }
+
+/**
+ * Add new keys parameters
+ * @param keyName The name of the key
+ * @param TTL The ttl of the key
+ * @param value The value of the key
+ * @param members The members of the key
+ * @param scores The scores of the key member
+ * @param field The field of the key
+ */
+export type AddNewKeyParameters = {
+  keyName: string
+  value?: string
+  TTL?: string
+  members?: string
+  scores?: string
+  field?: string
+  fields?: [
+    {
+      field?: string
+      valuse?: string
+    },
+  ]
+}
+
+/**
+* Hash key parameters
+* @param keyName The name of the key
+* @param fields The Array with fields
+* @param field The field of the field
+* @param value The value of the field
+
+*/
+export type HashKeyParameters = {
+  keyName: string
+  fields: {
+    field: string
+    value: string
+  }[]
+}
+
+/**
+ * Stream key parameters
+ * @param keyName The name of the key
+ * @param entries The Array with entries
+ * @param id The id of entry
+ * @param fields The Array with fields
+ */
+export type StreamKeyParameters = {
+  keyName: string
+  entries: {
+    id: string
+    fields: {
+      name: string
+      value: string
+    }[]
+  }[]
+}
+
+/**
+ * Set key parameters
+ * @param keyName The name of the key
+ * @param members The Array with members
+ */
+export type SetKeyParameters = {
+  keyName: string
+  members: string[]
+}
+
+/**
+ * Sorted Set key parameters
+ * @param keyName The name of the key
+ * @param members The Array with members
+ * @param name The name of the member
+ * @param id The id of the member
+ */
+export type SortedSetKeyParameters = {
+  keyName: string
+  members: {
+    name: string
+    score: number
+  }[]
+}
+
+/**
+ * List key parameters
+ * @param keyName The name of the key
+ * @param element The element in list
+ */
+export type ListKeyParameters = {
+  keyName: string
+  element: string
+}
+
+/**
+ * String key parameters
+ * @param keyName The name of the key
+ * @param value The value in the string
+ */
+export type StringKeyParameters = {
+  keyName: string
+  value: string
+}
+
+/**
+ * Json key parameters
+ * @param keyName The name of the key
+ * @param data The value in the json
+ */
+export type JsonKeyParameters = {
+  keyName: string
+  data: string
+}
+
+/**
+ * API response result
+ * @param T The type of the successful value
+ * @param E The type of the error
+ */
+export type Result<T, E> =
+  | { success: true; value: T }
+  | { success: false; error: E }
