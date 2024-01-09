21e12e15318b2507be45bb4ad916f3c774b2e6b5
Tsikhan Koktysh <tsikhan.koktysh@softeq.com>
2024-01-09 18:56:13 +0200

    RIVS-102 - fixed BE migration error

--- src/server/bootstrapBackend.ts
+++ src/server/bootstrapBackend.ts
@@ -4,8 +4,26 @@ import * as cp from 'child_process'
 import * as fs from 'fs'
 import { ChildProcessWithoutNullStreams } from 'child_process'
 
+const apiUrl = process.env.RI_BASE_API_URL
+const apiPort = process.env.RI_API_PORT
+const apiPrefix = process.env.RI_API_PREFIX
+
 const backendPath = path.join(__dirname, '..', 'redis-backend')
 let PSinst: ChildProcessWithoutNullStreams
+
+export function checkServerReady(callback: () => void) {
+  const checker = setInterval(() => {
+    fetch(`${apiUrl}:${apiPort}/${apiPrefix}/info`)
+      .then((res) => {
+        if (res.status === 200) {
+          clearInterval(checker)
+          callback()
+        }
+      }, () => {
+        console.debug('checking server...')
+      })
+  }, 1000)
+}
 
 export async function startBackend(): Promise<any> {
   return new Promise((resolve) => {
@@ -16,7 +34,7 @@ export async function startBackend(): Promise<any> {
       console.debug('Can\'t find backend folder')
       resolve('')
     } else {
-      const message = vscode.window.setStatusBarMessage('Starting RedisInsight backend...')
+      vscode.window.showInformationMessage('Starting RedisInsight backend...')
       if (process.platform === 'win32') {
         PSinst = cp.spawn('powershell.exe', [
           '-NoProfile',
@@ -24,21 +42,17 @@ export async function startBackend(): Promise<any> {
           '-NonInteractive',
           '-NoLogo',
           '-Command',
-          `$env:BUILD_TYPE="DOCKER_ON_PREMISE";node ${backendSrcPath}`,
+          // eslint-disable-next-line max-len
+          `$env:APP_FOLDER_NAME=".redisinsight-vsc";$env:NODE_ENV="production";$env:STDOUT_LOGGER="true";$env:BUILD_TYPE="DOCKER_ON_PREMISE";node ${backendSrcPath}`,
         ])
       } else {
         PSinst = cp.spawn(
           'node', [path.resolve(backendPath, 'src/main.js')],
-          { env: { BUILD_TYPE: 'DOCKER_ON_PREMISE', PATH: process.env.PATH } },
+          { env: { APP_FOLDER_NAME: '.redisinsight-vsc', NODE_ENV: 'production', BUILD_TYPE: 'DOCKER_ON_PREMISE', PATH: process.env.PATH } },
         )
       }
-      PSinst.stdout.on('data', (data: Buffer) => {
-        const infoData = data.toString()
-        console.debug(infoData)
-        if (infoData.includes('application successfully started')) {
-          message.dispose()
-          resolve('')
-        }
+      checkServerReady(() => {
+        resolve('')
       })
     }
   })
