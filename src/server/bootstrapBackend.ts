0bf549ba92f48de7a87e367198f3291742f758d0
Tsikhan Koktysh <tsikhan.koktysh@softeq.com>
2024-01-10 10:46:46 +0200

    fixed threads

--- src/server/bootstrapBackend.ts
+++ src/server/bootstrapBackend.ts
@@ -5,23 +5,25 @@ import * as fs from 'fs'
 import { ChildProcessWithoutNullStreams } from 'child_process'
 
 const apiUrl = process.env.RI_BASE_API_URL
+// TODO: add port avialability checker instead of hardcode
 const apiPort = process.env.RI_API_PORT
 const apiPrefix = process.env.RI_API_PREFIX
+const apiFolder = process.env.RI_API_FOLDER
 
 const backendPath = path.join(__dirname, '..', 'redis-backend')
 let PSinst: ChildProcessWithoutNullStreams
 
 export function checkServerReady(callback: () => void) {
-  const checker = setInterval(() => {
-    fetch(`${apiUrl}:${apiPort}/${apiPrefix}/info`)
-      .then((res) => {
-        if (res.status === 200) {
-          clearInterval(checker)
-          callback()
-        }
-      }, () => {
-        console.debug('checking server...')
-      })
+  const checker = setInterval(async () => {
+    try {
+      const res = await fetch(`${apiUrl}:${apiPort}/${apiPrefix}/info`)
+      if (res.status === 200) {
+        clearInterval(checker)
+        callback()
+      }
+    } catch (err) {
+      console.debug('checking server...')
+    }
   }, 1000)
 }
 
@@ -30,11 +32,11 @@ export async function startBackend(): Promise<any> {
     const backendSrcPath = path.join(backendPath, 'src/main.js')
 
     if (!fs.existsSync(backendPath)) {
-      vscode.window.showErrorMessage('Can\'t find backend folder')
-      console.debug('Can\'t find backend folder')
+      vscode.window.showErrorMessage('Can\'t find api folder')
+      console.debug('Can\'t find api folder')
       resolve('')
     } else {
-      vscode.window.showInformationMessage('Starting RedisInsight backend...')
+      const message = vscode.window.setStatusBarMessage('Starting RedisInsight...')
       if (process.platform === 'win32') {
         PSinst = cp.spawn('powershell.exe', [
           '-NoProfile',
@@ -43,15 +45,16 @@ export async function startBackend(): Promise<any> {
           '-NoLogo',
           '-Command',
           // eslint-disable-next-line max-len
-          `$env:APP_FOLDER_NAME=".redisinsight-vsc";$env:NODE_ENV="production";$env:STDOUT_LOGGER="true";$env:BUILD_TYPE="DOCKER_ON_PREMISE";node ${backendSrcPath}`,
+          `$env:APP_FOLDER_NAME=${apiFolder};$env:NODE_ENV="production";$env:STDOUT_LOGGER="true";$env:BUILD_TYPE="DOCKER_ON_PREMISE";node ${backendSrcPath}`,
         ])
       } else {
         PSinst = cp.spawn(
           'node', [path.resolve(backendPath, 'src/main.js')],
-          { env: { APP_FOLDER_NAME: '.redisinsight-vsc', NODE_ENV: 'production', BUILD_TYPE: 'DOCKER_ON_PREMISE', PATH: process.env.PATH } },
+          { env: { APP_FOLDER_NAME: apiFolder, NODE_ENV: 'production', BUILD_TYPE: 'DOCKER_ON_PREMISE', PATH: process.env.PATH } },
         )
       }
       checkServerReady(() => {
+        message.dispose()
         resolve('')
       })
     }
