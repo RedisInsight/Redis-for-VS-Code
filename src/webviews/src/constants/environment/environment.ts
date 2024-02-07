3667814b826ea0ea765488f21990993ad207852f
Zalenski Egor <63463140+zalenskiSofteq@users.noreply.github.com>
2024-02-08 00:00:40 +0100

    #RIVS-14 - Display list of the databases

--- src/webviews/src/constants/environment/environment.ts
+++ src/webviews/src/constants/environment/environment.ts
@@ -4,7 +4,6 @@ const port = Number(localStorage.getItem('apiPort'))
 export const BASE_API_URL = import.meta.env.RI_BASE_API_URL || 'http://localhost'
 export const API_PORT = port || import.meta.env.RI_API_PORT || 5541
 export const API_PREFIX = import.meta.env.RI_API_PREFIX || 'api'
-export const CONNECTED_DATABASE_ID = import.meta.env.RI_INSTANCE_ID || ''
 
 // browser
 export const SCAN_TREE_COUNT_DEFAULT = import.meta.env.RI_SCAN_TREE_COUNT || 10_000
--- src/webviews/src/pages/KeyDetailsPage/KeyDetailsPage.tsx
+++ src/webviews/src/pages/KeyDetailsPage/KeyDetailsPage.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { KeyDetails } from 'uiSrc/modules'
-import { useSelectedKeyStore } from 'uiSrc/store'
+import { ContextStoreProvider, useSelectedKeyStore } from 'uiSrc/store'
 
 export const KeyDetailsPage: FC = () => {
   const selectedKey = useSelectedKeyStore(useShallow((state) => state.data?.name))
 
   return (
-    <div className="p-1 h-full" data-testid="key-details-page">
-      <KeyDetails keyProp={selectedKey!} />
+    <div className="h-full" data-testid="key-details-page">
+      <ContextStoreProvider>
+        <KeyDetails keyProp={selectedKey!} />
+      </ContextStoreProvider>
     </div>
   )
 }
