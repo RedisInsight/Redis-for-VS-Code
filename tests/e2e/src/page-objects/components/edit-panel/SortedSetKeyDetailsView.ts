e4a26f8674144a16acbec8f9f8e3bcce58727940
vlad-dargel <u.darhel@aisnovations.com>
2024-02-20 15:33:39 +0100

    e2e/feature/RIVS-42-44_add_members_to_sets

--- tests/e2e/src/page-objects/components/edit-panel/SortedSetKeyDetailsView.ts
+++ tests/e2e/src/page-objects/components/edit-panel/SortedSetKeyDetailsView.ts
@@ -1,13 +1,13 @@
 import { By } from 'selenium-webdriver'
 import { DoubleColumnKeyDetailsView } from '@e2eSrc/page-objects/components/edit-panel/DoubleColumnKeyDetailsView'
 import { KeyTypesShort } from '@e2eSrc/helpers/constants'
+import { InputActions } from '@e2eSrc/helpers/common-actions'
 
 /**
  * Sorted Set Key details view
  */
 export class SortedSetKeyDetailsView extends DoubleColumnKeyDetailsView {
   scoreButton = By.xpath(`//*[@data-testid = 'header-sorting-button']`)
-
   sortedSetFieldsList = By.xpath(
     `//*[contains(@data-testid, 'zset-member-value-')]`,
   )
@@ -17,10 +17,10 @@ export class SortedSetKeyDetailsView extends DoubleColumnKeyDetailsView {
   scoreSortedSetFieldsList = By.xpath(
     `//*[contains(@data-testid, 'zset-score-value-')]`,
   )
-
   sortedSetScoreEditor = By.xpath(
     `//*[@id = 'score' and @data-testId = 'inline-item-editor']`,
   )
+  zsetMemberScoreInput = By.xpath(`//*[@data-testid = 'member-score']`)
 
   /**
    * Edit Sorted Set key value from details
@@ -34,5 +34,17 @@ export class SortedSetKeyDetailsView extends DoubleColumnKeyDetailsView {
       this.sortedSetScoreEditor,
       KeyTypesShort.ZSet,
     )
+  }
+
+  /**
+   * Add member to the ZSet key
+   * @param keyMember The value of the Zset member
+   * @param score The value of the score
+   */
+  async addMemberToZSet(keyMember: string, score: number): Promise<void> {
+    await (await this.getElement(this.addKeyValueItemsButton)).click()
+    await InputActions.typeText(this.setMemberInput, keyMember)
+    await InputActions.typeText(this.zsetMemberScoreInput, `${score}`)
+    await (await this.getElement(this.saveMemberButton)).click()
   }
 }
