ccc1bdb0d7b15543636283c295a917002df5c998
zalenskiSofteq <egor.zalenski@softeq.com>
2024-07-25 19:30:48 +0200

    #RIVS-188 - Display messages for automatic updates

---
+++ src/lib/update/checkVersionUpdate.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode'
+import { workspaceStateService } from '../workspace/workspaceState'
+import { EXTENSION_ID, EXTERNAL_LINKS } from '../../constants'
+import { logger } from '../../logger'
+
+export const checkVersionUpdate = async () => {
+  const previousVersion = workspaceStateService.get('extensionVersion')
+  const currentVersion = vscode.extensions.getExtension(EXTENSION_ID)?.packageJSON.version
+
+  logger.log(`Current version: ${currentVersion}`)
+  workspaceStateService.set('extensionVersion', currentVersion)
+
+  const linkText = vscode.l10n.t('Release Notes')
+  const message = vscode.l10n.t('Redis for VS Code extension updated to {0}.', currentVersion)
+
+  if (previousVersion && previousVersion !== currentVersion) {
+    const selection = await vscode.window.showInformationMessage(message, linkText)
+
+    if (selection === linkText) {
+      await vscode.env.openExternal(vscode.Uri.parse(EXTERNAL_LINKS.releaseNotes))
+    }
+  }
+}
