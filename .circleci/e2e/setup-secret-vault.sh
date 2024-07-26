e70722e606318c9762681d8f1a442e162dac7aae
ArtemHoruzhenko <artem.horuzhenko@redis.com>
2024-07-26 12:20:37 +0300

    poc

---
+++ .circleci/e2e/setup-secret-vault.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+GNOME_KEYRING_PASS=${GNOME_KEYRING_PASS:-'somepass'}
+echo "export GNOME_KEYRING_PASS=$GNOME_KEYRING_PASS" >> $BASH_ENV
+
+eval "$(dbus-launch --sh-syntax)"
+
+echo "export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" >> $BASH_ENV
+echo "DBUS_SESSION_BUS_PID=$DBUS_SESSION_BUS_PID" >> $BASH_ENV
+echo "DBUS_SESSION_BUS_WINDOWID=$DBUS_SESSION_BUS_WINDOWID" >> $BASH_ENV
+
+echo 'eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"' >> $BASH_ENV
+echo 'sleep 1' >> $BASH_ENV
+echo 'eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --start)"' >> $BASH_ENV
