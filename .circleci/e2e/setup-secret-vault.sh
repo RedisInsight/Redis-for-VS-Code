74b72f1996f7d0d37dd6cce0a58021188cb2007f
ArtemHoruzhenko <artem.horuzhenko@redis.com>
2024-07-26 11:50:03 +0300

    poc

--- .circleci/e2e/setup-secret-vault.sh
+++ .circleci/e2e/setup-secret-vault.sh
@@ -3,28 +3,13 @@ set -e
 
 GNOME_KEYRING_PASS=${GNOME_KEYRING_PASS:-'somepass'}
 
-echo "dbus-launch"
 eval "$(dbus-launch --sh-syntax)"
-echo "dbus launched"
 
 echo "export GNOME_KEYRING_PASS=$GNOME_KEYRING_PASS" >> $BASH_ENV
 echo "export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" >> $BASH_ENV
-echo "export DBUS_SESSION_BUS_PID=$DBUS_SESSION_BUS_PID" >> $BASH_ENV
-echo "export DBUS_SESSION_BUS_WINDOWID=$DBUS_SESSION_BUS_WINDOWID" >> $BASH_ENV
-echo "exported envs"
+echo "DBUS_SESSION_BUS_PID=$DBUS_SESSION_BUS_PID" >> $BASH_ENV
+echo "DBUS_SESSION_BUS_WINDOWID=$DBUS_SESSION_BUS_WINDOWID" >> $BASH_ENV
 
-echo "GNOME keyring unlock"
-eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"
-
-echo "export GNOME_KEYRING_CONTROL=$GNOME_KEYRING_CONTROL" >> $BASH_ENV
-echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> $BASH_ENV
-
-sleep 1
-
-echo "GNOME keyring start"
-eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --start)"
-
-source $BASH_ENV
-
-yarn --cwd kt
-node kt/index.js
+echo 'eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"' >> $BASH_ENV
+echo 'sleep 1' >> $BASH_ENV
+echo 'eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --start)"' >> $BASH_ENV
