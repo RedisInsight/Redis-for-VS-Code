1f6391d9243083b7f97aef0aecfbbc4faf7fc003
ArtemHoruzhenko <artem.horuzhenko@redis.com>
2024-07-26 11:14:54 +0300

    poc

--- .circleci/e2e/setup-secret-vault.sh
+++ .circleci/e2e/setup-secret-vault.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 
-export V4=4
-echo "V5=5" >> $BASH_ENV
-echo "export V6=6" >> $BASH_ENV
-
-echo "V4:$V4"
-echo "V5:$V5"
-echo "V6:$V6"
-
 GNOME_KEYRING_PASS=${GNOME_KEYRING_PASS:-'somepass'}
 
+echo "dbus-launch"
 eval "$(dbus-launch --sh-syntax)"
 
-echo "export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" >> $BASH_ENV
 echo "export GNOME_KEYRING_PASS=$GNOME_KEYRING_PASS" >> $BASH_ENV
+echo "export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS" >> $BASH_ENV
+echo "export DBUS_SESSION_BUS_PID=$DBUS_SESSION_BUS_PID" >> $BASH_ENV
+echo "export DBUS_SESSION_BUS_WINDOWID=DBUS_SESSION_BUS_WINDOWID" >> $BASH_ENV
 
 source $BASH_ENV
 
-mkdir -p ~/.cache
-mkdir -p ~/.local/share/keyrings
+#mkdir -p ~/.cache
+#mkdir -p ~/.local/share/keyrings
 
+echo "GNOME keyring unlock"
 eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"
 
 echo "export GNOME_KEYRING_CONTROL=$GNOME_KEYRING_CONTROL" >> $BASH_ENV
@@ -28,10 +24,10 @@ echo "export SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> $BASH_ENV
 
 source $BASH_ENV
 
-echo "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"
-echo "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"
-echo "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --unlock)"
-
 sleep 1
 
+echo "GNOME keyring start"
 eval "$(echo "$GNOME_KEYRING_PASS" | gnome-keyring-daemon --start)"
+
+yarn --cwd ky
+node kt/index.js
