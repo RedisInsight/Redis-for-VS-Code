0e86f74ee9f7958795e41dc536ca4e6e3a76cc1f
mariasergeenko <maria.sergeenko@softeq.com>
2024-08-14 13:44:09 +0200

    fix part of edit test

--- tests/e2e/src/page-objects/components/editor-view/KeyDetailsView.ts
+++ tests/e2e/src/page-objects/components/editor-view/KeyDetailsView.ts
@@ -51,8 +51,8 @@ export class KeyDetailsView extends WebView {
 
   getTrashIcon = (keyType: string, name: string): By =>
     By.xpath(
-      `//*[@data-testid="remove-${keyType}-button-${name}-icon"] 
-      | //*[@data-testid="${keyType}-remove-button-${name}-icon"] 
+      `//*[@data-testid="remove-${keyType}-button-${name}-icon"]
+      | //*[@data-testid="${keyType}-remove-button-${name}-icon"]
       | //*[@data-testid="${keyType}-remove-btn-${name}-icon"]
       | //*[@data-testid="remove-${keyType}-button-${name}-trigger"]
       | //*[@data-testid="${keyType}-remove-button-${name}-trigger"]`,
@@ -75,8 +75,8 @@ export class KeyDetailsView extends WebView {
     By.xpath(`//*[@data-testid[starts-with(., '${keyName.split('-')[0]}-') and contains(., '${columnName}')]]
     | //*[@data-testid[starts-with(., '${keyName.split('-')[0]}-field-') and contains(., '${columnName}')]]`)
   getHighlightedValue = (keyName: string, columnName: string): By =>
-    By.xpath(`//*[@data-testid[starts-with(., '${keyName.split('-')[0]}-') and contains(., '${columnName}')]]//*[@data-testid='value-as-json']
-    | //*[@data-testid[starts-with(., '${keyName.split('-')[0]}-field-') and contains(., '${columnName}')]]//*[@data-testid='value-as-json']`)
+    By.xpath(`//*[@data-testid[starts-with(., '${keyName.split('-')[0]}') and contains(., '${columnName}')]]//*[@data-testid='value-as-json']
+    | //*[@data-testid[starts-with(., '${keyName.split('-')[0]}_content-') and contains(., '${columnName}')]]//*[@data-testid='value-as-json']`)
   getEditBtnByKeyName = (keyName: string): By =>
     By.xpath(`//vscode-button[contains(@data-testid, 'edit-') and contains(@data-testid, '${keyName}')]`)
   /**
