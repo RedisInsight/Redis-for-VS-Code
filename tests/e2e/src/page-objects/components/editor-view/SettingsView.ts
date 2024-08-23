d43b83e9c8ee0e04161d02fe934c4fee38506677
vlad-dargel <u.darhel@aisnovations.com>
2024-08-23 16:40:22 +0200

    some fixes

--- tests/e2e/src/page-objects/components/editor-view/SettingsView.ts
+++ tests/e2e/src/page-objects/components/editor-view/SettingsView.ts
@@ -1,13 +1,22 @@
 import { By } from 'selenium-webdriver'
 import { WebView } from '@e2eSrc/page-objects/components/WebView'
-import { CheckboxActions } from '@e2eSrc/helpers/common-actions'
+import {
+  ButtonActions,
+  CheckboxActions,
+  InputActions,
+} from '@e2eSrc/helpers/common-actions'
+import { InputWithButtons } from '../common/InputWithButtons'
 
 /**
  * Settings view
  */
 export class SettingsView extends WebView {
-  switchAnalyticsOption = By.xpath(`//*[@data-testid='check-option-analytics']/..`)
-  switchAnalyticsCheckbox = By.xpath(`//input[@data-testid='check-option-analytics']/../../div[contains(@class, 'checkmarkContainer')]`)
+  switchAnalyticsOption = By.xpath(
+    `//*[@data-testid='check-option-analytics']/..`,
+  )
+  switchAnalyticsCheckbox = By.xpath(
+    `//input[@data-testid='check-option-analytics']/../../div[contains(@class, 'checkmarkContainer')]`,
+  )
   delimiterInput = By.xpath(`//input[@data-testid='input-delimiter']`)
 
   /**
@@ -16,5 +25,13 @@ export class SettingsView extends WebView {
   async getAnalyticsSwitcherValue(): Promise<boolean> {
     const switcherElement = await super.getElement(this.switchAnalyticsOption)
     return await CheckboxActions.getCheckboxState(switcherElement)
+  }
+
+  /**
+   * Set delimiter default value
+   */
+  async setDelimiterDefaultValue(): Promise<void> {
+    await InputActions.slowType(this.delimiterInput, ':')
+    await ButtonActions.clickElement(InputWithButtons.applyInput)
   }
 }
