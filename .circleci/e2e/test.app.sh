515273c83a1cee7d1cdafad40e69a721e834e654
vlad-dargel <u.darhel@aisnovations.com>
2024-08-26 15:20:33 +0200

    fix 5

--- .circleci/e2e/test.app.sh
+++ .circleci/e2e/test.app.sh
@@ -3,14 +3,19 @@ set -e
 
 yarn --cwd tests/e2e install
 
-# create folder before tests run to prevent permissions issue
+# Create folder before tests run to prevent permissions issues
 mkdir -p tests/e2e/remote
 
-# run rte
+# Run RTE (Redis Test Environment)
 docker-compose -f tests/e2e/rte.docker-compose.yml build
 docker-compose -f tests/e2e/rte.docker-compose.yml up --force-recreate -d -V
-./tests/e2e/wait-for-redis.sh localhost 12000 && \
+./tests/e2e/wait-for-redis.sh localhost 12000
 
-# run tests
-RI_SOCKETS_CORS=true \
-yarn --cwd tests/e2e dotenv -e .ci.env yarn --cwd tests/e2e test:ci
+# Run tests with the split files
+if [ -z "$TEST_FILES" ]; then
+  echo "No TEST_FILES specified, running all tests..."
+  RI_SOCKETS_CORS=true yarn --cwd tests/e2e dotenv -e .ci.env yarn --cwd tests/e2e test:ci
+else
+  echo "Running split tests: $TEST_FILES"
+  RI_SOCKETS_CORS=true yarn --cwd tests/e2e dotenv -e .ci.env yarn --cwd tests/e2e test:ci --testFiles="$TEST_FILES"
+fi
