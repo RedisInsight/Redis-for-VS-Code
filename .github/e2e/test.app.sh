37410f5172c109728f57478243e0bdf6a80d5bb2
Egor Zalenski <egor.zalenski@softeq.com>
2024-10-29 13:37:56 +0100

    test

--- .github/e2e/test.app.sh
+++ .github/e2e/test.app.sh
@@ -10,5 +10,6 @@ docker compose -f tests/e2e/rte.docker-compose.yml up --force-recreate -d -V
 ./tests/e2e/wait-for-redis.sh localhost 12000
 
 # Run tests
+# RI_SOCKETS_CORS=true yarn --cwd tests/e2e dotenv -e .ci.env yarn --cwd tests/e2e test:ci
 RI_SOCKETS_CORS=true \
 yarn --cwd tests/e2e dotenv -e .ci.env yarn --cwd tests/e2e test:ci
