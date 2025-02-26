53cd2f6a9d25f527a3eca16ebae374d3ace14ce8
Dijana Antovska <dijana.antovska@redis.com>
2025-02-26 15:46:19 +0200

    add tests for the added store methods

--- src/webviews/src/store/hooks/use-oauth/useOAuthStore.spec.ts
+++ src/webviews/src/store/hooks/use-oauth/useOAuthStore.spec.ts
@@ -1,4 +1,3 @@
-import * as modules from 'uiSrc/modules'
 import { CloudJobName, CloudJobStatus } from 'uiSrc/constants'
 import { constants } from 'testSrc/helpers'
 import { waitForStack } from 'testSrc/helpers/testUtils'
@@ -7,7 +6,9 @@ import {
   initialOAuthState,
   fetchUserInfo,
   createFreeDbJob,
+  fetchPlans,
 } from './useOAuthStore'
+import { CloudSubscriptionPlanResponse } from './interface'
 
 beforeEach(() => {
   useOAuthStore.setState(initialOAuthState)
@@ -38,6 +39,7 @@ describe('useOAuthStore', () => {
     // Assert
     expect(useOAuthStore.getState().user.loading).toEqual(false)
   })
+
   it('getUserInfoSuccess', () => {
     // Arrange
     const initialState = { ...initialOAuthState, loading: true } // Custom initial state
@@ -48,6 +50,62 @@ describe('useOAuthStore', () => {
     getUserInfoSuccess(constants.USER_DATA)
     // Assert
     expect(useOAuthStore.getState().user.data).toEqual(constants.USER_DATA)
+  })
+
+  it('setIsRecommendedSettingsSSO', () => {
+    const initialState = { ...initialOAuthState, plan: { isRecommendedSettings: true } }
+    useOAuthStore.setState((state) => ({ ...state, ...initialState }))
+
+    const { setIsRecommendedSettingsSSO } = useOAuthStore.getState()
+
+    setIsRecommendedSettingsSSO(false)
+
+    expect(useOAuthStore.getState().isRecommendedSettings).toEqual(false)
+  })
+
+  it('getPlans', () => {
+    const initialState = { ...initialOAuthState, plan: { loading: false } }
+    useOAuthStore.setState((state) => ({ ...state, ...initialState }))
+
+    const { getPlans } = useOAuthStore.getState()
+
+    getPlans()
+
+    expect(useOAuthStore.getState().plan.loading).toEqual(true)
+  })
+
+  it('getPlansSuccess', () => {
+    const initialState = { ...initialOAuthState, plan: { loading: true, data: [] } }
+    useOAuthStore.setState((state) => ({ ...state, ...initialState }))
+
+    const { getPlansSuccess } = useOAuthStore.getState()
+
+    getPlansSuccess(constants.PLANS_DATA as CloudSubscriptionPlanResponse[])
+
+    expect(useOAuthStore.getState().plan.data).toEqual(constants.PLANS_DATA)
+    expect(useOAuthStore.getState().plan.loading).toEqual(false)
+  })
+
+  it('getPlansFailure', () => {
+    const initialState = { ...initialOAuthState, plan: { loading: true } }
+    useOAuthStore.setState((state) => ({ ...state, ...initialState }))
+
+    const { getPlansFailure } = useOAuthStore.getState()
+
+    getPlansFailure()
+
+    expect(useOAuthStore.getState().plan.loading).toEqual(false)
+  })
+
+  it('setIsOpenSelectPlanDialog', () => {
+    const initialState = { ...initialOAuthState, plan: { isOpenDialog: false } }
+    useOAuthStore.setState((state) => ({ ...state, ...initialState }))
+
+    const { setIsOpenSelectPlanDialog } = useOAuthStore.getState()
+
+    setIsOpenSelectPlanDialog(true)
+
+    expect(useOAuthStore.getState().plan.isOpenDialog).toEqual(true)
   })
 })
 
@@ -67,6 +125,15 @@ describe('async', () => {
 
     expect(useOAuthStore.getState().job).toEqual(
       { id: constants.USER_JOBS_DATA.id, name, status: CloudJobStatus.Running },
+    )
+  })
+
+  it('fetchPlans', async () => {
+    fetchPlans()
+    await waitForStack()
+
+    expect(useOAuthStore.getState().plan).toEqual(
+      { isOpenDialog: true, data: constants.PLANS_DATA, loading: false },
     )
   })
 })
