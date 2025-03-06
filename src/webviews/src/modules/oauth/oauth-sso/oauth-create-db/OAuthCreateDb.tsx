4067caf74166c9f306077e9621155e5f480c9c2f
Dijana Antovska <dijana.antovska@redis.com>
2025-03-06 12:44:34 +0200

    introduce a LOGIN_EVERY_TIME constant, as there is no logout functionality and the user should be able to create free DBs with different accounts

--- src/webviews/src/modules/oauth/oauth-sso/oauth-create-db/OAuthCreateDb.tsx
+++ src/webviews/src/modules/oauth/oauth-sso/oauth-create-db/OAuthCreateDb.tsx
@@ -10,6 +10,7 @@ import { createFreeDbJob, fetchCloudSubscriptionPlans, useOAuthStore } from 'uiS
 import { Spacer } from 'uiSrc/ui'
 import { INFINITE_MESSAGES } from 'uiSrc/components'
 
+import { LOGIN_EVERY_TIME } from './constants'
 import { OAuthForm } from '../../shared/oauth-form'
 import OAuthAgreement from '../../shared/oauth-agreement/OAuthAgreement'
 import { OAuthAdvantages, OAuthRecommendedSettings } from '../../shared'
@@ -85,7 +86,7 @@ const OAuthCreateDb = (props: Props) => {
         <OAuthAdvantages />
       </div>
       <div className={styles.socialContainer}>
-        {!data ? (
+        {!data || LOGIN_EVERY_TIME ? (
           <OAuthForm
             className={styles.socialButtons}
             onClick={handleSocialButtonClick}
---
+++ src/webviews/src/modules/oauth/oauth-sso/oauth-create-db/constants.ts
@@ -0,0 +1,4 @@
+// TODO [DA]: Remove once logout is implemented and it is decided that this functionality no longer needed
+// Added in order to give flexibility to the user to login every time and change cloud accounts
+// on each create free db event
+export const LOGIN_EVERY_TIME: boolean = true
