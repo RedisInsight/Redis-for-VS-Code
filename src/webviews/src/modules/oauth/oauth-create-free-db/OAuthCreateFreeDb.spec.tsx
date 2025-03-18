1ec013fb5ead798411aa855d9a93fe5f2767b5d8
Dijana Antovska <dijana.antovska@redis.com>
2025-03-18 11:32:13 +0200

    add one more test case for the non compressed case; ehnahce / update existing ones

--- src/webviews/src/modules/oauth/oauth-create-free-db/OAuthCreateFreeDb.spec.tsx
+++ src/webviews/src/modules/oauth/oauth-create-free-db/OAuthCreateFreeDb.spec.tsx
@@ -4,7 +4,7 @@ import * as utils from 'uiSrc/utils'
 import { OAuthSocialAction, OAuthSocialSource, VscodeMessageAction } from 'uiSrc/constants'
 import { initialOAuthState, useOAuthStore } from 'uiSrc/store'
 import { vscodeApi } from 'uiSrc/services'
-import { cleanup, fireEvent, render } from 'testSrc/helpers'
+import { cleanup, fireEvent, render, waitFor } from 'testSrc/helpers'
 import OAuthCreateFreeDb from './OAuthCreateFreeDb'
 
 vi.spyOn(utils, 'sendEventTelemetry')
@@ -13,7 +13,8 @@ vi.spyOn(vscodeApi, 'postMessage')
 beforeEach(() => {
   useOAuthStore.setState({
     ...initialOAuthState,
-    source: 'source',
+    source: null,
+    ssoFlow: undefined,
   })
   cleanup()
   vi.resetAllMocks()
@@ -21,24 +22,24 @@ beforeEach(() => {
 
 describe('OAuthConnectFreeDb', () => {
   it('should render if there is a free cloud db', () => {
-    const { queryByTestId } = render(<OAuthCreateFreeDb source={OAuthSocialSource.ListOfDatabases}/>)
+    const { queryByTestId } = render(<OAuthCreateFreeDb source={OAuthSocialSource.AddDbForm}/>)
     expect(queryByTestId('create-free-db-btn')).toBeInTheDocument()
   })
 
   it('should send telemetry after click on connect btn', async () => {
-    const { queryByTestId } = render(<OAuthCreateFreeDb source={OAuthSocialSource.ListOfDatabases} />)
+    const { queryByTestId } = render(<OAuthCreateFreeDb source={OAuthSocialSource.DatabasesList} />)
 
     fireEvent.click(queryByTestId('create-free-db-btn') as HTMLButtonElement)
 
     expect(sendEventTelemetry).toBeCalledWith({
       event: TelemetryEvent.CLOUD_FREE_DATABASE_CLICKED,
       eventData: {
-        source: OAuthSocialSource.ListOfDatabases,
+        source: OAuthSocialSource.DatabasesList,
       },
     })
   })
 
-  it('should open page and oauth sso modal compressed button is clicked', () => {
+  it('should open add database page and oauth sso modal when compressed button is clicked', () => {
     const { queryByTestId } = render(<OAuthCreateFreeDb source={OAuthSocialSource.DatabasesList} compressed={true}/>)
 
     const compressedCreateBtn = queryByTestId('create-free-db-btn')
@@ -53,5 +54,29 @@ describe('OAuthConnectFreeDb', () => {
         source: OAuthSocialSource.DatabasesList,
       },
     })
+
+    const state = useOAuthStore.getState()
+    const socialDialog = queryByTestId('social-oauth-dialog')
+    waitFor(() => {
+      expect(state.ssoFlow).toEqual(OAuthSocialAction.Create)
+      expect(state.isOpenSocialDialog).toEqual(true)
+      expect(socialDialog).toBeInTheDocument()
+    })
+  })
+
+  it('should open auth sso modal when non compressed button is clicked', () => {
+    const { queryByTestId } = render(<OAuthCreateFreeDb source={OAuthSocialSource.DatabasesList} compressed={false}/>)
+
+    const regularCreateBtn = queryByTestId('create-free-db-btn')
+    expect(regularCreateBtn).toBeInTheDocument()
+
+    fireEvent.click(regularCreateBtn as HTMLButtonElement)
+
+    const state = useOAuthStore.getState()
+    expect(state.ssoFlow).toEqual(OAuthSocialAction.Create)
+    expect(state.isOpenSocialDialog).toEqual(true)
+
+    const socialDialog = queryByTestId('social-oauth-dialog')
+    waitFor(() => expect(socialDialog).toBeInTheDocument())
   })
 })
