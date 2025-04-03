3f22cf9b5373aa735f25a8ee457bc9ee3291624b
Dijana Antovska <dijana.antovska@redis.com>
2025-04-03 15:21:32 +0300

    when json module is missing, instead of opening the try-free link, change to open the oauth sso dialog by passing the callback fn to the helper text

--- src/webviews/src/constants/window/helpTexts.tsx
+++ src/webviews/src/constants/window/helpTexts.tsx
@@ -8,11 +8,15 @@ import { EXTERNAL_LINKS, UTM_CAMPAIGNS } from 'uiSrc/constants'
 import styles from 'uiSrc/components/popover-delete/styles.module.scss'
 
 export const helpTexts = {
-  REJSON_SHOULD_BE_LOADED: (
+  REJSON_SHOULD_BE_LOADED: (onFreeTrialDbClick: () => void) => (
     <>
-      {l10n.t('This database does not support the JSON data structure. Learn more about JSON support ')}
+      {l10n.t(
+        'This database does not support the JSON data structure. Learn more about JSON support ',
+      )}
       <Link
-        to={getUtmExternalLink(EXTERNAL_LINKS.jsonModule, { campaign: UTM_CAMPAIGNS.tutorials })}
+        to={getUtmExternalLink(EXTERNAL_LINKS.jsonModule, {
+          campaign: UTM_CAMPAIGNS.tutorials,
+        })}
         className="underline hover:no-underline"
         target="_blank"
         data-test-subj="no-json-module-info"
@@ -20,14 +24,14 @@ export const helpTexts = {
         {l10n.t('here. ')}
       </Link>
       {l10n.t('You can also create a ')}
-      <Link
-        to={getUtmExternalLink(EXTERNAL_LINKS.tryFree, { campaign: UTM_CAMPAIGNS.redisjson })}
+      <a
+        onClick={onFreeTrialDbClick}
         className="underline hover:no-underline"
-        target="_blank"
         data-test-subj="no-json-module-try-free"
+        href="#"
       >
         {l10n.t('free trial Redis Cloud database')}
-      </Link>
+      </a>
       {l10n.t(' with built-in JSON support.')}
     </>
   ),
@@ -35,15 +39,22 @@ export const helpTexts = {
     <div className={styles.appendInfo}>
       <VscWarning className="mr-1 mt-1" />
       <span>
-        {l10n.t('If you remove the single {0}, the whole Key will be deleted.', fieldType)}
+        {l10n.t(
+          'If you remove the single {0}, the whole Key will be deleted.',
+          fieldType,
+        )}
       </span>
     </div>
   ),
   REMOVING_MULTIPLE_ELEMENTS_NOT_SUPPORT: (
     <>
-      {l10n.t('Removing multiple elements is available for Redis databases v. 6.2 or later. Update your Redis database or create a new ')}
+      {l10n.t(
+        'Removing multiple elements is available for Redis databases v. 6.2 or later. Update your Redis database or create a new ',
+      )}
       <Link
-        to={getUtmExternalLink(EXTERNAL_LINKS.tryFree, { campaign: UTM_CAMPAIGNS.redisLatest })}
+        to={getUtmExternalLink(EXTERNAL_LINKS.tryFree, {
+          campaign: UTM_CAMPAIGNS.redisLatest,
+        })}
         className="underline hover:no-underline"
         target="_blank"
         data-test-subj="no-json-module-try-free"
